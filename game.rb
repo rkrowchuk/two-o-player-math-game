a6134dcbdb921bedcbe8d39d67601a6636e57d57
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-04 13:05:12 -0700

    adding math to question

--- game.rb
+++ game.rb
@@ -8,8 +8,12 @@ class Game
     @player_2 = Player.new("Player 2")    
   end
 
+  def to_s
+    "Welcome! #{@player_1.name} goes first."
+  end
+
   def start 
-    puts "Welcome #{@player_1} and #{@player_2}!"
+    puts self.to_s
     question_1 = Question.new
     puts question_1.ask
   end
--- question.rb
+++ question.rb
@@ -1,12 +1,21 @@
 class Question 
+  attr_accessor :turn, :num1, :num2
+
   def initialize
     @turn = 1
   end
 
   def ask
-    puts "What is 1 + 1?"
+    @num1 = rand(1..10)
+    @num2 = rand(1..10)
+    puts "------NEW TURN------"
+    puts "What is #{num1} + #{num2}?"
     print "> "
-    answer = $stdin.gets.chomp
+    answer = $stdin.gets.chomp.to_i
+    if answer == (num1 + num2)
+      puts "Correct!"
+    else puts "Sorry, you're wrong. You lost a life."
+    end
   end 
 
 
